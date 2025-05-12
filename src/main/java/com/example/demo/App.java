c38c2773e6cba7ab1a7a07828f5bbf95c7114d90
Pavithra Manivannan <Pavithra.Manivannan@hansencx.com>
2025-05-12 10:28:42 +1000

    Initial commit

--- src/main/java/com/example/demo/App.java
+++ src/main/java/com/example/demo/App.java
@@ -2,6 +2,6 @@ package com.example.demo;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Developer!");
+        System.out.println("Hello, Developer! Welcome to Tech Talk");
     }
 }
