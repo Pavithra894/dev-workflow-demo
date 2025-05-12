a06155dcd8ec322c546e1b882e48d22c59813e5e
Pavithra Manivannan <Pavithra.Manivannan@hansencx.com>
2025-05-12 10:47:00 +1000

    Testing hook

--- src/main/java/com/example/demo/App.java
+++ src/main/java/com/example/demo/App.java
@@ -3,5 +3,6 @@ package com.example.demo;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, Developer! Welcome to Tech Talk");
+        System.out.println("This is a simple Java application.);
     }
 }
