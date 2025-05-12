b2adf34d49ee365c9867745ebde79bfd6876ad74
Pavithra Manivannan <Pavithra.Manivannan@hansencx.com>
2025-05-12 11:07:38 +1000

    Checkstyle test

--- src/main/java/com/example/demo/App.java
+++ src/main/java/com/example/demo/App.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Developer! Welcome to Tech Talk");
-        System.out.println("This is a simple Java application.);
+    System.out.println("Hello, Developer! Welcome to Tech Talk");
+    System.out.println("This is a simple Java application.");
     }
 }
